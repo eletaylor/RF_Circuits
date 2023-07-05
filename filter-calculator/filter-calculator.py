fe8deddd8e129afa6c1e79e1df0bccb1875dd72d
Eleanor <t.ellie118@gmail.com>
2023-07-05 08:02:50 -0600

    moved LPF transformation to its own fxn

--- filter-calculator/filter-calculator.py
+++ filter-calculator/filter-calculator.py
@@ -4,15 +4,21 @@
     This is a program to help calculate filter transformation values.
 
     Created:    22 June 2023
-    Modified:    4 July 2023
+    Modified:    5 July 2023
     
 '''
 
-PI = 3.1415
-
 import textwriter
+import transform_known_LPF
 
 def main():
+
+    # List of all values tracked by this filter
+    keys_list = ["f_0", "w_0", "Z_0", "bw_Hz", "bw_rad", 
+                 "L_lp", "C_lp", "L_hp", "C_hp", 
+                 "L_bp_series", "C_bp_series", "L_bp_parallel", "C_bp_parallel", 
+                 "L_bs_parallel", "C_bs_parallel", "L_bs_series", "C_bs_series"]
+    filter_values = {}
 
     print ("\n#######################################################################")
     print ("\nWELCOME TO FILTER CALCULATOR - Your one-stop shop for all filter needs!")
@@ -28,24 +34,19 @@ def main():
                                 transmission line filter with no known components"""))
 
         match mode:
-            case 1:
-                L_lp = float(input("Enter the lowpass filter inductor value in H:\t"))
-                C_lp = float(input("Enter the lowpass filter capacitor value in F:\t"))
-            case 2: # Page 735
-                L_lp = 15.9155e-9 * (1e9/f_0) 
-                C_lp = 6.3662e-12 * (1e9/f_0)
-            case 3: # Page 735
-                Z_0 = float(input("Enter the characteristic impedance in Ohms:\t"))
-                L_lp = 15.9155e-9 * (1e9/f_0) 
-                C_lp = 6.3662e-12 * (1e9/f_0)
+            case 1: # TODO: Calculate Z_0 from these values
+                filter_values = transform_known_LPF(keys_list)
+            #case 2: # Page 735
+                #L_lp = 15.9155e-9 * (1e9/f_0) 
+                #C_lp = 6.3662e-12 * (1e9/f_0)
+            #case 3: # Page 735 # TODO: Scale all by Z_0
+                #Z_0 = float(input("Enter the characteristic impedance in Ohms:\t"))
+                #L_lp = 15.9155e-9 * (1e9/f_0) 
+                #C_lp = 6.3662e-12 * (1e9/f_0)
             # case 4: # Find all m-derived components with no known components, variable impedance and m-value
                 #This will require some changes to be made to textwriter to accomodate more components
             case _:
                 mode = -1
-
-    f_0 = float(input("Enter the cutoff frequency in Hz:\t"))
-    bw_Hz = float(input("Enter the desired bandwidth in Hz:\t"))
-    
     
     # For a transmission line, Z_0 = sqrt(L/C)
     # If we include parasitics, Z_0 = sqrt((R + jwL)/(G + jwC))
@@ -80,31 +81,9 @@ def main():
 
     # Table 22.5 gives butterworth values, use equations 31 and 32
     # Table 22.7 gives chebyshev values
-
-    w_0 = f_0 * 2 * PI
-    bw_rad = bw_Hz * 2 * PI
-
-    # Find the global L and C values for transformation
-    L = L_lp * w_0
-    C = C_lp * w_0
-
-    # Find the highpass filter values from the global LC
-    L_hp = 1/(w_0 * L)
-    C_hp = 1/(w_0 * C)
-
-    # Find the bandpass filter values from the global LC
-    L_bp_series = L/bw_rad
-    C_bp_series = bw_rad/(w_0 * w_0 * L)
-    L_bp_parallel = bw_rad/(w_0 * w_0 * C)
-    C_bp_parallel = C/bw_rad
-
-    # Find the bandstop filter values from the global LC
-    L_bs_parallel = (bw_rad * L) / (w_0 * w_0)
-    C_bs_parallel = 1/(bw_rad * L)
-    L_bs_series = 1/(bw_rad * C)
-    C_bs_series = (bw_rad * C) / (w_0 * w_0)
     
     # Save as a list of strings to be iterable
+    '''
     text = textwriter(f_0, bw_Hz, L_lp, C_lp, L_hp, C_hp, L_bp_series, C_bp_series, L_bp_parallel, C_bp_parallel, L_bs_parallel, C_bs_parallel, L_bs_series, C_bs_series)
             
     filename = f"filters_{int(f_0)}Hz.txt"
@@ -115,6 +94,6 @@ def main():
             textfile.write("\n")    # Add a newline character
 
     print(f"Transformation values saved to: {filename}")
-
+    '''
 if __name__ == "__main__":
     main()
